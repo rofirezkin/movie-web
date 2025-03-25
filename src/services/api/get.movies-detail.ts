17dfa2ba876792a733846165a42f31e85733ca5c
rofirezkin <rrezkin@pvg.co.id>
2025-03-26 03:09:37 +0700

    fix

--- src/services/api/get.movies-detail.ts
+++ src/services/api/get.movies-detail.ts
@@ -1,15 +1,11 @@
-import { buildQueryParams } from "../generate-query-params";
 import { http } from "../interceptor";
 import { MovieDetail } from "./get.movie-detail";
-import { MovieResponse } from "./get.movies.type";
 
 export type MovieDetailRequest = {
-  id?: string ;
- 
+  id?: string;
 };
 
 export async function getMovieDetail(payload: MovieDetailRequest) {
-
   return await http.get<MovieDetail>(`&i=${payload.id}`);
 }
 
